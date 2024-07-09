7ca1b19d591630e80030bf8d11e4bf686c4bcdf7
Narotam-Mishra <narotam21jan@gmail.com>
2024-07-09 10:38:20 +0530

    backend-dev-prisma_1.6_added_login_logout_middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,10 +28,62 @@ const signup = async (req, res, next) => {
         // send user a token
         cookieToken(user, res)
     } catch (error) {
+        if (error.code === 'P2002' && error.meta && error.meta.target.includes('email')) {
+            // Handle unique constraint violation
+            return res.status(400).json({
+                message: 'Email already exists. Please use a different email.'
+            });
+        }
+        next(error);  // Pass other errors to the error handler
+    }
+}
+
+// login user
+const login = async (req, res) => {
+    try {
+        // take info from user
+        const { email, password } = req.body;
+
+        if(!email || !password){
+            throw new Error('Please provide email and password fields correctly');
+        }
+
+        // find a user based on email
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        })
+
+        // when there is no user
+        if(!user){
+            throw new Error('User not found!!');
+        }
+
+        // password mismatch
+        if(user.password !== password){
+            throw new Error('password is incorrect!!');
+        }
+
+        // user exist and it is validated
+        cookieToken(user, res)
+    } catch (error) {
         throw new Error(error)
     }
 }
 
+// logout user
+const logout = async (req, res) => {
+    try {
+        res.clearCookie('token');
+        res.json({
+            success: true
+        })
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
 module.exports = {
-    signup
+    signup, login, logout
 }
